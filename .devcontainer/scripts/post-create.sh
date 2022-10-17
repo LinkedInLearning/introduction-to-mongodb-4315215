d0a5f15fa7a4bab18f3450cf1f5651f8b2c5bbbd
Justin Jenkins <justinjenkins@gmail.com>
2022-10-17 23:49:33 +0000

    cleanup

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,7 +1,9 @@
 # add alias for mongo client which is no longer available
 echo alias mongo="\"echo -e '\\n>> NOTE: 📣📣 Use the \033[1mmongosh\033[0m command instead! 📣📣\\n'\"" >> ~/.bashrc
 # start up mongosh without warnings
-echo alias mongosh="mongosh --quiet"
+echo alias mongosh="mongosh --quiet" >> ~/.bashrc
+
+ln -s /workspace/extras/.mongoshrc.js ~/.mongoshrc.js
 
 # remove some warnings
 mongosh --eval "disableTelemetry()"
---
+++ extras/.mongoshrc.js
@@ -0,0 +1,1 @@
+print("🔥 Welcome to Introduction to MongoDB!");
