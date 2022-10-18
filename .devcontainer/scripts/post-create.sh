6b7d04c64c41acb1a201f5353f583f43325af101
Justin Jenkins <justinjenkins@gmail.com>
2022-10-18 01:30:11 +0000

    🐳

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,7 +1,7 @@
 # add alias for mongo client which is no longer available
 echo alias mongo="\"echo -e '\\n>> NOTE: 📣📣 Use the \033[1mmongosh\033[0m command instead! 📣📣\\n'\"" >> ~/.bashrc
 # start up mongosh without warnings
-echo alias mongosh="mongosh --quiet" >> ~/.bashrc
+echo alias mongosh="\"mongosh --quiet\"" >> ~/.bashrc
 
 ln -s /workspace/extras/.mongoshrc.js ~/.mongoshrc.js
 
@@ -10,8 +10,8 @@ mongosh --eval "disableTelemetry()"
 mongosh --eval "db.disableFreeMonitoring()"
 
 # install drivers
-sh mongodb-go-drivers.sh
-sh mongodb-node-drivers.sh
+/bin/bash /workspace/.devcontainer/scripts/mongodb-go-drivers.sh
+/bin/bash /workspace/.devcontainer/scripts/mongodb-node-drivers.sh
 
 # import example data
-sh import-examples.sh
+/bin/bash /workspace/.devcontainer/scripts/import-examples.sh
--- extras/.mongoshrc.js
+++ extras/.mongoshrc.js
@@ -1,1 +1,9 @@
-print("🔥 Welcome to Introduction to MongoDB!");
+console.clear();
+print("🔥 Welcome to Introduction to MongoDB! 🔥");
+print("");
+print("🔍 See README.md files in the /lessons/ folder.")
+print("");
+print("📝 Switched to the `cooker` database");
+print("");
+
+db = db.getSiblingDB('cooker');
