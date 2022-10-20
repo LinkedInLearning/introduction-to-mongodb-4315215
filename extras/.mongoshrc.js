9004de51b0cc99d140c36b5bb7dde4b37c6d63aa
Justin Jenkins <justinjenkins@gmail.com>
2022-10-20 19:03:38 +0000

    cleanup mongoshrc

--- extras/.mongoshrc.js
+++ extras/.mongoshrc.js
@@ -1,7 +1,18 @@
+/* This mongoshrs files was created to clean up unnessicary warnings, etc.
+ when connecting to MongoDB. Feel free to customize this file but note it
+ can break the shell if there is anything invalid.
+ */
+
+const connectionString = db.getMongo();
+
 console.clear();
+
 print("🔥 Welcome to Introduction to MongoDB! 🔥");
 print("");
-print("🔍 See README.md files in the /lessons/ folder.")
+print("🔍 See README.md files in the /lessons/ folders.")
+print("");
+console.log("Connecting to:   ", connectionString);
+print(`Using MongoDB:    ${db.version()}`);
 print("");
 print("📝 Switched to the `cooker` database");
 print("");
